737602be57e78f72005f430fe56ba497fa52568c
sunaot <sunao.tanabe@gmail.com>
2016-04-26 01:00:15 +0900

    Make Rack app skippable

--- lib/rack/skippable.rb
+++ lib/rack/skippable.rb
@@ -1,7 +1,29 @@
 require "rack/skippable/version"
 
+# @example
+#
+#   skip_when = ->(env) {
+#     request = Rack::Request.new(env)
+#     return request.path == '/img'
+#   }
+#   use Rack::Skippable.wrap(Rack::Auth::Basic, skip_when) do |u, p|
+#     expected = [ENV['NAME'], ENV['PASSWORD']]
+#     expected == [u, p]
+#   end
 module Rack
   module Skippable
-    # Your code goes here...
+    def self.wrap(base_class, skip_when)
+      rack_class = Class.new(base_class)
+      rack_class.class_exec(skip_when) do |skip_when|
+        define_method(:call) do |env|
+          if skip_when.call(env)
+            @app.call(env)
+          else
+            super(env)
+          end
+        end
+      end
+      rack_class
+    end
   end
 end
--- test/rack/skippable_test.rb
+++ test/rack/skippable_test.rb
@@ -1,11 +1,42 @@
 require 'test_helper'
 
+class RackModule
+  class Called < StandardError; end
+  attr_accessor :app
+
+  def call(env)
+    main_logic
+  end
+
+  def main_logic
+    raise Called
+  end
+end
+
 class Rack::SkippableTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Rack::Skippable::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_skip_when_condition_is_truthy
+    skip_when = ->(env) { return true }
+    r = rack_app(skip_when)
+    assert_equal 'skipped', r.call({})
+  end
+
+  def test_not_skip_when_condition_is_falsey
+    skip_when = ->(env) { return nil }
+    r = rack_app(skip_when)
+    assert_raises(RackModule::Called) do
+      r.call({})
+    end
+  end
+
+  private
+  def rack_app(skip_when, app: proc { 'skipped' })
+    rack_class = ::Rack::Skippable.wrap(RackModule, skip_when)
+    r = rack_class.new
+    r.app = app
+    r
   end
 end
